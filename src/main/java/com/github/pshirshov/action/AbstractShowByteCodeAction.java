98b5a4115cf7ebe61d60031fcf8abb49fe2bb44a
XenoAmess <xenoamess@gmail.com>
2024-01-01 23:56:57 +0800

    add runReadAction, fix read failure

--- src/main/java/com/github/pshirshov/action/AbstractShowByteCodeAction.java
+++ src/main/java/com/github/pshirshov/action/AbstractShowByteCodeAction.java
@@ -109,21 +109,27 @@ public abstract class AbstractShowByteCodeAction extends AnAction {
 
             @Override
             public void run(@NotNull ProgressIndicator indicator) {
-                if (ProjectRootManager.getInstance(project).getFileIndex()
-                        .isInContent(virtualFile) && isMarkedForCompilation(project, virtualFile)) {
-                    myErrorMessage = "Unable to show bytecode for '" + psiElementTitle + "'. Class file does not " +
-                            "exist or is out-of-date.";
-                    myErrorTitle = "Class File Out-Of-Date";
-                } else {
-                    myByteCode = ApplicationManager.getApplication().runReadAction(new Computable<String>() {
-                        @Override
-                        public String compute() {
-                            return new BytecodeConverter(
-                                    AbstractShowByteCodeAction.this.getDisassembleStrategy()
-                            ).getByteCode(psiElement);
+                ApplicationManager.getApplication().runReadAction(new Computable<String>() {
+                    @Override
+                    public String compute() {
+                        if (ProjectRootManager.getInstance(project).getFileIndex()
+                                .isInContent(virtualFile) && isMarkedForCompilation(project, virtualFile)) {
+                            myErrorMessage = "Unable to show bytecode for '" + psiElementTitle + "'. Class file does not " +
+                                    "exist or is out-of-date.";
+                            myErrorTitle = "Class File Out-Of-Date";
+                        } else {
+                            myByteCode = ApplicationManager.getApplication().runReadAction(new Computable<String>() {
+                                @Override
+                                public String compute() {
+                                    return new BytecodeConverter(
+                                            AbstractShowByteCodeAction.this.getDisassembleStrategy()
+                                    ).getByteCode(psiElement);
+                                }
+                            });
                         }
-                    });
-                }
+                        return myByteCode;
+                    }
+                });
             }
 
 
